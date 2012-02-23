697ec01edaba34c5711d7da8fc5fb071029f72e2
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2012-02-23 19:51:21 +0100

    add a module for git::checkout

---
+++ manifests/checkout.pp
@@ -0,0 +1,60 @@
+# handle git checkouts from a repository
+
+# set up known_hosts and identities outside of this module
+
+define git::checkout ($directory, $repository, $user=undef, $commit='master') {
+
+    notify {"git::checkout of $repository to $directory": }
+
+    file {
+        "$directory":
+            ensure  => directory,
+            mode    => 0755,
+            owner   => $user,
+    }
+
+    $allrequire = [
+        File["$directory"],
+    ]
+
+    if ($user) {
+        $require = [$allrequire, User[$user], ]
+    } else {
+        $require = [$allrequire, ]
+    }
+
+    # only run if the .git directory does not exist
+    exec {
+        "git-clone-$directory":
+            cwd         => $directory,
+            user        => $user,
+            command     => "/usr/bin/git clone --recursive $repository .",
+            creates     => "$directory/.git",
+            refreshonly => false,
+            logoutput   => on_failure,
+            require     => $require,
+    }
+
+    # always run
+    exec {
+        "git-pull-$directory":
+            cwd         => $directory,
+            user        => $user,
+            command     => "/usr/bin/git pull; /usr/bin/git submodule init; /usr/bin/git submodule update",
+            refreshonly => false,
+            logoutput   => on_failure,
+            require     => $require,
+    }
+
+    # FIXME: only run if the commit is given and different from current checkout
+    exec {
+        "git-checkout-$directory":
+            cwd         => $directory,
+            user        => $user,
+            command     => "/usr/bin/git checkout $commit",
+            # unless      => " ",
+            refreshonly => false,
+            logoutput   => true,
+            require     => Exec["git-pull-$directory"],
+    }
+}
