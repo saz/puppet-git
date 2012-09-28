8498b2d6fb77007ed80a07338ea3afbed826d381
Steffen Zieger <me@saz.sh>
2012-09-28 13:21:20 +0200

    style guide changes

--- manifests/checkout.pp
+++ manifests/checkout.pp
@@ -23,7 +23,7 @@
 #   Clone and checkout or update to a given commit or tag.
 #
 # == Sample Usage:
-# 
+#
 #   git::checkout {'linux kernel':
 #       directory => '/usr/src',
 #       checkoutdir => 'linux',
@@ -32,100 +32,96 @@
 #   }
 #
 define git::checkout (
-    $directory, $checkoutdir, $repository,
-    $user=undef, $commit='master') {
+  $directory,
+  $checkoutdir,
+  $repository,
+  $user=undef,
+  $commit='master'
+) {
 
-    include git::install
+  include git::install
 
-    file {
-        "$directory":
-            ensure  => directory,
-            mode    => 0755,
-            owner   => $user,
-    }
+  file { $directory:
+    ensure  => directory,
+    mode    => '0755',
+    owner   => $user,
+  }
 
-    $allrequire = [
-        File["$directory"],
-        Class['git::install'],
-    ]
+  $allrequire = [
+    File[$directory],
+    Class['git::install'],
+  ]
 
-    if ($user) {
-        $require = [$allrequire, User[$user], ]
-    } else {
-        $require = [$allrequire, ]
-    }
+  if ($user) {
+    $require = [$allrequire, User[$user], ]
+  } else {
+    $require = [$allrequire, ]
+  }
 
-    # only run if the .git directory does not exist
-    exec {
-        "git-clone-$directory":
-            cwd         => $directory,
-            user        => $user,
-            path        => [ "/bin", "/usr/bin", ],
-            command     => "git clone --recursive $repository $checkoutdir && cd $checkoutdir && git checkout $commit",
-            creates     => "$directory/$checkoutdir/.git",
-            refreshonly => false,
-            logoutput   => on_failure,
-            require     => $require,
-    }
+  # only run if the .git directory does not exist
+  exec { "git-clone-${directory}":
+    cwd         => $directory,
+    user        => $user,
+    path        => [ '/bin', '/usr/bin', ],
+    command     => "git clone --recursive ${repository} ${checkoutdir} && cd ${checkoutdir} && git checkout ${commit}",
+    creates     => "${directory}/${checkoutdir}/.git",
+    refreshonly => false,
+    logoutput   => on_failure,
+    require     => $require,
+  }
 
-    # FIXME: only run if the commit is given and different from current checkout
-    # FIXME: but if we check out a branch, always fetch and update
-    # FIXME: if it's a branch, we need to do git pull too
-    exec {
-        "git-checkout-$directory":
-            cwd         => "$directory/$checkoutdir",
-            user        => $user,
-            path        => [ "/bin", "/usr/bin", ],
-            # Note:
-            # $commit can be
-            # - a local branch (which will not be updated)
-            # - a remote branch (which will be updated)
-            # - a tag (tags are fetched so will be recent)
-            # - a commit hash
+  # FIXME: only run if the commit is given and different from current checkout
+  # FIXME: but if we check out a branch, always fetch and update
+  # FIXME: if it's a branch, we need to do git pull too
+  exec { "git-checkout-${directory}":
+    cwd         => "${directory}/${checkoutdir}",
+    user        => $user,
+    path        => [ "/bin", "/usr/bin", ],
+    refreshonly => false,
+    logoutput   => on_failure,
+    require     => Exec["git-clone-${directory}"],
+    # Note:
+    # $commit can be
+    # - a local branch (which will not be updated)
+    # - a remote branch (which will be updated)
+    # - a tag (tags are fetched so will be recent)
+    # - a commit hash
 
-            # to keep updating from a branch, use origin/(branch name)
-            # FIXME: if git pull creates new files that are already present,
-            #        a pull fails, and so we don't have newer commits
-            #        so instead we fetch, then do a detached checkout of
-            #        $commit, and since git checkout has --force it overwrites
-            #        those files
-            command     => 
-                "git fetch -a \
-                    && git fetch -t \
-                    && git checkout --force $commit \
-                    && git submodule init \
-                    && git submodule sync \
-                    && git submodule update --recursive \
-                    && git rev-parse HEAD > ../commit",
-            # if the unless command has an exit value of 1, command will run
+    # to keep updating from a branch, use origin/(branch name)
+    # FIXME: if git pull creates new files that are already present,
+    #        a pull fails, and so we don't have newer commits
+    #        so instead we fetch, then do a detached checkout of
+    #        $commit, and since git checkout has --force it overwrites
+    #        those files
+    command     => "git fetch -a \
+      && git fetch -t \
+      && git checkout --force ${commit} \
+      && git submodule init \
+      && git submodule sync \
+      && git submodule update --recursive \
+      && git rev-parse HEAD > ../commit",
 
-            # if there is no commit file, run
-            # if there is a commit file, but it matches $commit (and hence is
-            # an actual commit hash), don't run
-            # if commit is master or a branch name, we need to fetch because
-            #    origin may have changed
-            # run if whatever commit hash the $commit points to does not match
-            #    our current commit file
+    # if the unless command has an exit value of 1, command will run
 
-            # test on machine with:
-            # su - www
-            # cd /var/www/merchant-test.credex.net
-            # export commit=master
-            # copy-paste unless command
-            # echo $?
-            unless      => 
-                "test -e ../commit && \
-                  ( \
-                    test x$commit == `cat ../commit` || \
-                        ( \
-                            git fetch -a; \
-                            test `git rev-parse --verify $commit^0 | head -n 1` == `cat ../commit` ) )",
+    # if there is no commit file, run
+    # if there is a commit file, but it matches $commit (and hence is
+    # an actual commit hash), don't run
+    # if commit is master or a branch name, we need to fetch because
+    #    origin may have changed
+    # run if whatever commit hash the $commit points to does not match
+    #    our current commit file
 
-            refreshonly => false,
-            logoutput   => on_failure,
-            require     => [
-                Exec["git-clone-$directory"],
-#                File["$directory/$checkoutdir"],
-            ]
-    }
+    # test on machine with:
+    # su - www
+    # cd /var/www/merchant-test.credex.net
+    # export commit=master
+    # copy-paste unless command
+    # echo $?
+    unless      => "test -e ../commit && \
+      ( \
+      test x${commit} == `cat ../commit` || \
+      ( \
+      git fetch -a; \
+      test `git rev-parse --verify ${commit}^0 | head -n 1` == `cat ../commit` ) )",
+  }
 }
